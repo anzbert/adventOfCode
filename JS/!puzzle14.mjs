2a1ecea0a9e7da6cdd483c060141cdc0fdcaa0b4
anzbert <38823700+anzbert@users.noreply.github.com>
2021-08-29 18:54:32 +0800

    conversion added

--- JS/!puzzle14.mjs
+++ JS/!puzzle14.mjs
@@ -1,5 +1,12 @@
 import fs from "fs";
 
+function msToKph(ms) {
+  return ms * 3.6;
+}
+function kphToMs(kmh) {
+  return kmh / 3.6;
+}
+
 const inputFile = fs.readFileSync(
   new URL("../data/2015-puzzle14.txt", import.meta.url),
   "utf8"
@@ -10,10 +17,11 @@ console.log("all instructions:", inputArray.length);
 
 const splitArr = inputArray.map((string) => string.split(" "));
 
-// format: [ Reindeer, speed in km/h , maximal duration in s, resting time in s ]
+// format: [ Reindeer, speed in km/h , speed in m/s, maximal duration in s, resting time in s ]
 const cleanedArr = splitArr.map((arr) => [
   arr[0],
   parseInt(arr[3]),
+  kphToMs(parseInt(arr[3])),
   parseInt(arr[6]),
   parseInt(arr.at(-2)),
 ]);
