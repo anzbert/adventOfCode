38f29432042c51a78fd44bb5802b7461612d8ca0
anzbert <38823700+anzbert@users.noreply.github.com>
2021-09-02 15:35:40 +0800

    .

--- src/rusty16_2.rs
+++ src/rusty16_2.rs
@@ -1,11 +1,9 @@
-use std::fs;
-
 pub fn run() {
-    let input = match fs::read_to_string("./data/2016-puzzle2.txt") {
+    let input = match std::fs::read_to_string("./data/2016-puzzle2.txt") {
         Ok(content) => content,
         Err(err) => panic!("{:?}", err),
     };
-    println!("input: {}", input);
+    // println!("input: {}", input);
 
     let test = "ULLRRDDDLURDLUUUUD"; // result 5
 }
