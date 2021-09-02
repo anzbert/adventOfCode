0addf96189ca0c80bef9ebc69a416c17014edde3
anzbert <38823700+anzbert@users.noreply.github.com>
2021-09-02 16:03:00 +0800

    progressed on 16-2

--- src/rusty16_2.rs
+++ src/rusty16_2.rs
@@ -6,4 +6,23 @@ pub fn run() {
     // println!("input: {}", input);
 
     let test = "ULLRRDDDLURDLUUUUD"; // result 5
+    let start_pos = vec![1, 1]; // is number 5
+
+    println!("test {}", grid_3x3_to_num(&start_pos));
+    println!("final posi: {:?}", plot_direction(&test, &start_pos));
+}
+
+fn plot_direction(input: &str, input_pos: &Vec<i32>) -> i32 {
+    let mut pos = input_pos;
+
+    for dir in input.chars() {
+        println!("char {}", dir);
+    }
+
+    grid_3x3_to_num(&pos)
+}
+
+fn grid_3x3_to_num(grid_pos: &Vec<i32>) -> i32 {
+    let num = grid_pos[0] + 1 + grid_pos[1] * 3;
+    num
 }
