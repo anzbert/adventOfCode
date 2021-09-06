e73f80da88cdda0d69fde412210a02fc72c72ff1
anzbert <38823700+anzbert@users.noreply.github.com>
2021-09-06 23:17:19 +0800

    part one 16-3

--- src/rusty16_3.rs
+++ src/rusty16_3.rs
@@ -1,1 +1,49 @@
-pub fn run() {}
+use regex::Regex;
+
+pub fn run() {
+    // triangle inequality theorem:
+    // the sum of any 2 sides of a trinagle must be greater than the third.
+
+    #[allow(unused_variables)] // disable unused variable warning
+    let input = match std::fs::read_to_string("./data/2016-puzzle3.txt") {
+        Ok(content) => content,
+        Err(err) => panic!("{:?}", err),
+    };
+
+    let test1 = "    5   10   25"; // -> invalid
+                                   // parse(test1);
+
+    let test2 =
+        "101 301 501 \n 102 302 502 \n 103 303 503 \n 201 401 601 \n 202 402 602 \n 203 403 603";
+    // test answer is 6
+
+    let split: Vec<&str> = test2.split("\n").collect();
+
+    let mut parsed: Vec<Vec<u32>> = Vec::new();
+    for i in &split {
+        parsed.push(parse(i));
+    }
+    println!("parsed: {:?}", parsed);
+
+    let mut counter = 0;
+    for t in &parsed {
+        if t[0] + t[1] > t[2] && t[0] + t[2] > t[1] && t[1] + t[2] > t[0] {
+            counter += 1;
+        }
+    }
+    println!("counter: {} / {}", counter, parsed.len()); // RESULT ONE: 983
+
+    // PART TWO:
+}
+
+fn parse(input: &str) -> Vec<u32> {
+    let re = Regex::new(r"\d+").unwrap();
+    let mut result: Vec<u32> = Vec::new();
+    for n in re.find_iter(input) {
+        let text = n.as_str();
+        let num: u32 = text.parse().unwrap();
+        result.push(num);
+    }
+    // println!("got: {:?}", result);
+    result
+}
