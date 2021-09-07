02d5b39a6ebe1a8b4ffce2bd202d23d93cdf610d
anzbert <38823700+anzbert@users.noreply.github.com>
2021-09-07 23:11:11 +0800

    .

--- JS/!puzzle16.mjs
+++ JS/!puzzle16.mjs
@@ -5,7 +5,37 @@ const inputFile = fs.readFileSync(
   "utf8"
 );
 const inputArray = inputFile.split("\n");
-console.log("aunts:", inputArray.length);
+console.log("aunts input:", inputArray.length);
+
+let test = "Sue 1: cars: 9, akitas: 3, goldfish: 0";
+
+const sueRegEx = /Sue (\d+)/;
+
+const attributeRegExObj = {
+  children: /children: (\d+)/,
+  cats: /cats: (\d+)/,
+  samoyeds: /samoyeds: (\d+)/,
+  pomeranians: /pomeranians: (\d+)/,
+  akitas: /akitas: (\d+)/,
+  vizslas: /vizslas: (\d+)/,
+  goldfish: /goldfish: (\d+)/,
+  trees: /trees: (\d+)/,
+  cars: /cars: (\d+)/,
+  perfumes: /perfumes: (\d+)/,
+};
+
+let aunts = [];
+inputArray.forEach((sue) => {
+  let matches = {};
+  Object.keys(attributeRegExObj).forEach((key) => {
+    let match = sue.match(attributeRegExObj[key]);
+    if (match) matches[key] = parseInt(match[1]);
+  });
+  aunts.push(matches);
+});
+
+console.log("aunts array of properties:", aunts.length);
+console.log(aunts);
 
 /*
 CLUE:
