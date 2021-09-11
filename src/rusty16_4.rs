c82337625f2e8b1cfde153ebedb7ffa011d3b122
anzbert <38823700+anzbert@users.noreply.github.com>
2021-09-11 13:50:01 +0800

    FINISHED PART ONE

--- src/rusty16_4.rs
+++ src/rusty16_4.rs
@@ -14,7 +14,7 @@ pub fn run() {
         Ok(content) => content,
         Err(err) => panic!("{:?}", err),
     };
-    let input: Vec<&str> = input_string.split("\r\n").collect();
+    let input: Vec<&str> = input_string.split("\n").collect();
     let test = vec![
         "aaaaa-bbb-z-y-x-123[abxyz]", // is a real room because the most common letters are a (5), b (3), and then a tie between x, y, and z, which are listed alphabetically.
         "a-b-c-d-e-f-g-h-987[abcde]", // is a real room because although the letters are all tied (1 of each), the first five are listed alphabetically.
@@ -23,7 +23,7 @@ pub fn run() {
     ];
 
     // println!("split in: {:#?}", test);
-    let no_dashes: Vec<String> = test
+    let no_dashes: Vec<String> = input
         .iter()
         .map(|x| x.to_string().replace("-", ""))
         .map(|x| x.replace("]", ""))
@@ -53,57 +53,51 @@ pub fn run() {
         })
         .collect();
 
-    // println!("dfsfsdf {:#?}", done);
+    println!("prepared input {:#?}", done);
 
     let mut counter = 0;
     for i in done {
-        println!("testing {:?}", i.code);
         counter += make_checksum(i);
     }
     println!("\ncorrect ones: {}\n", counter);
 
-    // 162501 wrong
-    // 1344 wrong
-}
-
-fn _check_entry(entry: Code) -> usize {
-    let checksum: Vec<&str> = entry.checksum.split("").filter(|x| !x.is_empty()).collect();
-
-    let mut buffer = 9999;
-    for c in checksum.iter() {
-        print!("letter: \"{}\" ", c);
-        let matching_letters: usize = entry.code.matches(c).collect::<Vec<_>>().len();
-        println!("appears {:#?} times", matching_letters);
-        if matching_letters > buffer || matching_letters == 0 {
-            println!("hot damn!!");
-            return 0;
-        }
-        if matching_letters <= buffer {
-            buffer = matching_letters;
-        }
-    }
-    println!("good one..");
-    entry.num
+    // RESULT PART ONE: 158835
 }
 
 fn make_checksum(entry: Code) -> usize {
-    let control_checksum = entry.checksum;
-    // let mut letters = HashMap::new();
+    println!("testing {:?} - value: {}", entry.code, entry.num);
+    let mut letters = HashMap::new();
 
-    // for ch in entry.code.chars() {
-    //     let counter = letters.entry(ch).or_insert(0);
-    //     *counter += 1;
-    // }
+    for ch in entry.code.chars() {
+        let counter = letters.entry(ch).or_insert(0);
+        *counter += 1;
+    }
 
-    let mut to_sort: Vec<char> = entry.code.chars().collect();
-    to_sort.sort();
+    let mut coll: Vec<char> = Vec::new();
+    for i in (1..99).rev() {
+        let mut new: Vec<_> = letters
+            .iter()
+            .filter(|entry| *entry.1 == i)
+            .map(|x| *x.0)
+            .collect();
+        new.sort();
+        coll.append(&mut new);
+    }
 
-    println!("HM: {:?}", to_sort);
+    let mut to_sort: Vec<char> = coll.into_iter().collect();
 
-    let checksum: String = "oarel".to_string(); // placeholder
-    if control_checksum.eq(&checksum) {
+    to_sort.dedup();
+    to_sort.resize(5, 'z');
+
+    let checksum: String = to_sort.iter().collect();
+    println!("provided checksum: {:?}", entry.checksum);
+    print!("  -> created checksum: {:?} -> ", checksum);
+
+    if entry.checksum.eq(&checksum) {
+        println!("match! add {}\n", entry.num);
         return entry.num;
     } else {
+        println!("no match...\n");
         return 0;
     }
 }
